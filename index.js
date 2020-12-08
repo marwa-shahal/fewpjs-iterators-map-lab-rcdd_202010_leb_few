c38632defae71e6a464b0f19b096ead3d9c5ee02
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-08 20:31:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tutorials = [
 
 
 function titleCased() {
-  return tutorials.map(tut =>{return tut.split(' ').map(t => t.charAt(0).toUpperCase() + t.substring(1))
+  return tutorials.map(tut =>{return tut.split(' ').map(t => t.charAt(0).toUpperCase() + t.substring(1));
 });
  }
 
